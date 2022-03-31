4ba50b08b4ea2ff6ff8baa636514922be1a53335
wilfredvelotio <wilfred.fernandes@velotio.com>
2022-03-31 11:19:25 +0530

    updated action-creator

--- src/redux/actions/action-creator.ts
+++ src/redux/actions/action-creator.ts
@@ -4,21 +4,62 @@ import { Action, ActionModal, ActionPost, ActionTodo } from "./index";
 import axios from "axios";
 import { InitialValuesFormikUser } from "src/Components/MyForms/MyForms";
 import { getAxios, putAxios } from "src/Components/Reusable/Network/AxiosAllMethods";
-import { resetPost, resetTodo, resetUser } from "./reset";
 import { InitialValuesFormikPost } from "src/Components/MyForms/Posts";
 import { InitialValuesFormikTodos } from "src/Components/MyForms/Todos";
 import { isTodo, isUser } from "src/utils/helper";
+import { ReduxState } from "src/redux/reducers";
 
 export const fetchUsers = (url: string, pageStart: number, pageLimit: number) => {
-  return async (dispatch: Dispatch<Action>) => {
+  return async (dispatch: Dispatch<Action>, getState: () => ReduxState) => {
     const data = await getAxios<Props[]>(url);
     if (data)
       dispatch({
         type: ActionTypes.FETCH_USERS,
         payload: {
-          data: data,
-          pageStart: pageStart,
-          pageLimit: pageLimit,
+          data: [...getState().user.data, ...data],
+          pageStart: pageStart + data.length,
+          pageLimit: pageLimit + data.length,
+          didFirstLoad: true,
+        },
+      });
+    else
+      dispatch({
+        type: ActionTypes.LIMIT_REACHED,
+      });
+  };
+};
+
+export const fetchPosts = (url: string, pageStart: number, pageLimit: number) => {
+  return async (dispatch: Dispatch<ActionPost>, getState: () => ReduxState) => {
+    const data = await getAxios<MyPostProps[]>(url);
+    if (data)
+      dispatch({
+        type: ActionTypes.FETCH_POSTS,
+        payload: {
+          data: [...getState().posts.data, ...data],
+          pageStart: pageStart + data.length,
+          pageLimit: pageLimit + data.length,
+          didFirstLoad: true,
+        },
+      });
+    else {
+      dispatch({
+        type: ActionTypes.LIMIT_REACHED,
+      });
+    }
+  };
+};
+
+export const fetchTodos = (url: string, pageStart: number, pageLimit: number) => {
+  return async (dispatch: Dispatch<ActionTodo>, getState: () => ReduxState) => {
+    const data = await getAxios<MyTodosProps[]>(url);
+    if (data)
+      dispatch({
+        type: ActionTypes.FETCH_TODOS,
+        payload: {
+          data: [...getState().todos.data, ...data],
+          pageStart: pageStart + data.length,
+          pageLimit: pageLimit + data.length,
           didFirstLoad: true,
         },
       });
@@ -39,31 +80,9 @@ export const updateUsers = (udata: InitialValuesFormikUser) => {
   };
 };
 
-export const fetchPosts = (url: string, pageStart: number, pageLimit: number) => {
-  return async (dispatch: Dispatch<ActionPost>) => {
-    const data = await getAxios<MyPostProps[]>(url);
-    if (data)
-      dispatch({
-        type: ActionTypes.FETCH_POSTS,
-        payload: {
-          data: data,
-          pageStart: pageStart,
-          pageLimit: pageLimit,
-          didFirstLoad: true,
-        },
-      });
-    else {
-      dispatch({
-        type: ActionTypes.LIMIT_REACHED,
-      });
-    }
-  };
-};
-
 export const updatePosts = (udata: InitialValuesFormikPost) => {
   return async (dispatch: Dispatch<ActionPost>) => {
     const data = await putAxios<MyPostProps>(`posts/${udata.id}`, udata);
-    console.log(`posts/${udata.id}`, udata, data);
     dispatch({
       type: ActionTypes.UPDATE_POSTS,
       payload: data,
@@ -71,30 +90,9 @@ export const updatePosts = (udata: InitialValuesFormikPost) => {
   };
 };
 
-export const fetchTodos = (url: string, pageStart: number, pageLimit: number) => {
-  return async (dispatch: Dispatch<ActionTodo>) => {
-    const data = await getAxios<MyTodosProps[]>(url);
-    if (data)
-      dispatch({
-        type: ActionTypes.FETCH_TODOS,
-        payload: {
-          data: data,
-          pageStart: pageStart,
-          pageLimit: pageLimit,
-          didFirstLoad: true,
-        },
-      });
-    else
-      dispatch({
-        type: ActionTypes.LIMIT_REACHED,
-      });
-  };
-};
-
 export const updateTodos = (udata: InitialValuesFormikTodos) => {
   return async (dispatch: Dispatch<ActionTodo>) => {
     const data = await putAxios<MyTodosProps>(`todos/${udata.id}`, udata);
-    console.log(`posts/${udata.id}`, udata, data);
     dispatch({
       type: ActionTypes.UPDATE_TODOS,
       payload: data,
